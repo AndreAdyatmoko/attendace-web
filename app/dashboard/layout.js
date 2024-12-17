6451a1d99119bbd7f188e962b5b2db92046fb62c
AndreAdyatmoko <AndreAdyatmoko>
2024-12-18 02:45:26 +0700

    Adding Night mode feature

---
+++ app/dashboard/layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SideNav from "./_components/SideNav";
+import Header from "./_components/Header";
+
+function layout({ children }) {
+  return (
+    <div>
+      <div className="md:w-64 fixed hidden md:block">
+        <SideNav />
+      </div>
+      <div className="md:ml-64">{children}</div>
+    </div>
+  );
+}
+
+export default layout;
